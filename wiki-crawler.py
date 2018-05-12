0397ee34954b0a191a67a31a9508a24bd2675bbd
Alan Tan <alanwuha91@gmail.com>
2018-05-12 15:50:32 +0800

    Extracted list of popular movies using pycorpora and split entries by title and year

---
+++ wiki-crawler.py
@@ -0,0 +1,9 @@
+import pycorpora
+
+# Extract list of popular movies
+movies = pycorpora.film_tv.popular_movies['popular-movies']
+
+# Split entries into title and year
+movies = [m[:-1].split(' (') for m in movies ]
+
+print('Hello World!')
