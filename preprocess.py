125578cdba2b707c642c5579d2002c98df6ad8f2
Alan Tan <alanwuha91@gmail.com>
2018-05-16 10:00:42 +0800

    Converted content to lower case, extracted words using nltk tokenizer and removed english stop words using nltk stopwords corpus

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,17 @@
 import os
 import re
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+# import nltk
+# nltk.download()
 
 # Directories
 raw_dir = './data/raw/'
 preprocess_dir = './data/preprocess/'
+
+# Get stop words
+stop_words = set(stopwords.words('english'))
 
 # Preprocess txt files
 for file_name in os.listdir(raw_dir):
@@ -19,16 +27,18 @@ for file_name in os.listdir(raw_dir):
 
 			# Remove punctuation and multiple spaces using regular expression
 			content = re.sub(r'[^\w\s]', ' ', content)
-			content = ' '.join(content.split())
 
-			# Extract words
-			words = content.split(' ')
+			# Convert to lower case
+			content = content.lower()
 
-			# Perform preprocessing
-			# Remove punctuation, stop list, symbols...
-			words = [word + '\n' for word in words]
+			# Tokenize
+			word_tokens = word_tokenize(content)
+
+			# Remove stop words
+			words = [w for w in word_tokens if not w in stop_words]
 
 			# Write processed data to file
+			words = [word + '\n' for word in words]
 			with open(preprocess_path, 'w', encoding='utf-8') as f:
 				f.writelines(words)
 				print('\tPreprocessing completed and written to ' + preprocess_path)
